05a87ea035b50bcdbd5d7b90990e67840a582bfc
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-06-08 10:02:29 -0400

    Connect file loading workflow to menu system

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ def main():
 
     while userOption != 5:
         displayMenu()
-        userOption = pyip.inputInt("Select option (1-5): ", min=1, max=5)
 
-        if userOption == 5:
+        userOption = pyip.inputInt(
+            prompt="Select an option (1-5): ",
+            min=1,
+            max=5
+        )
+
+        if userOption == 1:
+            print("Processing spreadsheets...")
+            files = getFilePath()
+
+        elif userOption == 5:
             print("Goodbye!")
 
 if __name__ == "__main__":
